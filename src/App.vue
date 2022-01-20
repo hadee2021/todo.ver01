eec2abbfe2f880604b9e8aa7a146eaa816c046c0
hadee2021 <tpghks9920@naver.com>
2022-01-20 17:40:02 +0900

    todo-ver01

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <router-link class="navbar-brand" :to="{ name: 'Home'}">
+    Home
+  </router-link>
+  
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <router-link class="nav-link" :to="{ name: 'Todos'}">
+      Todos
+      </router-link>
+    </li>
+  </ul>
+</nav>
+<div class="container">
+  <router-view/>
+</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label>{{ label }}</label>
+      <input 
+        :value="subject" 
+        @input="onInput"
+        type="text" 
+        class="form-control"
+      >
+      <div 
+        v-if="error"
+        style="color: red"
+      >
+        {{error}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    error: {
+      type: String,
+      required: true
+    },
+    subject: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props, { emit }) {
+    const onInput = (e) => {
+      emit('update:subject', e.target.value)
+    }
+
+    return {
+      onInput,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .text-red {
+    color: red;
+  }
+</style>
